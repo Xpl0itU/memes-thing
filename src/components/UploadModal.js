e0764454a4122dfdc9778268842129fc5a8fb3db
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-10-05 16:08:53 +0200

    Simplify code

--- src/components/UploadModal.js
+++ src/components/UploadModal.js
@@ -6,45 +6,34 @@ import uploadtoInstagram from "../handlers/instagram_upload";
 export default function UploadModal(props) {
   const [show, setShow] = useState("close");
 
-  const handleShowConfirmModal = () => {
-    setShow("confirm")
-  }
-
-  const handleShowUploadedModal = () => {
-    setShow("uploaded")
-  }
-
-  const handleClose = () => {
-    setShow("close")
-  }
   return (
     <>
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={handleShowConfirmModal}>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => setShow("confirm")}>
         Upload
       </button>
 
-      <Modal show={show === "confirm"} onHide={handleClose}>
+      <Modal show={show === "confirm"} onHide={() => setShow("close")}>
         <Modal.Header closeButton>
           <Modal.Title>Upload this image?</Modal.Title>
         </Modal.Header>
         <Modal.Body>Please confirm to continue</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={() => setShow("close")}>
             Close
           </Button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => {uploadtoInstagram(props.token, props.image, props.caption, handleShowUploadedModal);}}>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => {uploadtoInstagram(props.token, props.image, props.caption, () => setShow("uploaded"));}}>
             Continue
           </button>
         </Modal.Footer>
       </Modal>
 
-      <Modal show={show === "uploaded"} onHide={handleClose}>
+      <Modal show={show === "uploaded"} onHide={() => setShow("close")}>
         <Modal.Header closeButton>
           <Modal.Title>Uploaded successfully!</Modal.Title>
         </Modal.Header>
         <Modal.Body>The image has been successfully uploaded</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={() => setShow("close")}>
             Close
           </Button>
         </Modal.Footer>
