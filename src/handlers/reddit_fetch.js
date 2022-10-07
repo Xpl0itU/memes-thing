7bf06fae4a6e5b592c46c33087353fe25e89a24a
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-10-07 21:45:51 +0200

    Remove pinned and stickied posts

--- src/handlers/reddit_fetch.js
+++ src/handlers/reddit_fetch.js
@@ -2,11 +2,15 @@ function isImage(url) {
     return url.endsWith('.jpg') && url.includes('redd');
 }
 
+function isPinned(url) {
+    return !url.pinned && !url.stickied;
+}
+
 function getSub(sub) {
     const request = new XMLHttpRequest();
     request.open('GET', `https://www.reddit.com/r/${sub}/hot.json?limit=20`, false);
     request.send(null);
-    return JSON.parse(request.response).data.children.map(post=>post.data.url).filter(isImage);
+    return JSON.parse(request.response).data.children.map(post=>post.data).filter(isPinned).map(post=>post.url).filter(isImage);
 }
 
 export default getSub;
