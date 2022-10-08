9a1f14a1883b7bc5428b828c8cae3bdf7332162f
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-10-08 15:48:33 +0200

    Use enums for states

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,17 +2,22 @@ import React, {useState} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Modal} from 'react-bootstrap';
 
+const showStates = {
+  SHOW: "show",
+  CLOSE: "close"
+}
+
 export default function Image(props) {
-  const [show, setShow] = useState("close");
+  const [show, setShow] = useState(showStates.CLOSE);
 
   return (
     <>
-      <Modal show={show === "show_image"} onHide={() => setShow("close")}>
+      <Modal show={show === showStates.SHOW} onHide={() => setShow(showStates.CLOSE)}>
         <Modal.Header closeButton/>
         <Modal.Body><img src={props.src} alt="" className="rounded-xl" height='500' width='500'/></Modal.Body>
       </Modal>
 
-      <img src={props.src} alt="" className="rounded-xl" height='200' width='200' onClick={() => setShow('show_image')}/>
+      <img src={props.src} alt="" className="rounded-xl" height='200' width='200' onClick={() => setShow(showStates.SHOW)}/>
     </>
   );
 }
--- src/components/UploadModal.js
+++ src/components/UploadModal.js
@@ -3,37 +3,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal, Button } from 'react-bootstrap';
 import uploadtoInstagram from "../handlers/instagram_upload";
 
+const showStates = {
+  CONFIRM: "confirm",
+  UPLOADED: "uploaded",
+  CLOSE: "close"
+}
+
 export default function UploadModal(props) {
-  const [show, setShow] = useState("close");
+  const [show, setShow] = useState(showStates.CLOSE);
 
   return (
     <>
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => setShow("confirm")}>
+      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => setShow(showStates.CONFIRM)}>
         Upload
       </button>
 
-      <Modal show={show === "confirm"} onHide={() => setShow("close")}>
+      <Modal show={show === showStates.CONFIRM} onHide={() => setShow(showStates.CLOSE)}>
         <Modal.Header closeButton>
           <Modal.Title>Upload this image?</Modal.Title>
         </Modal.Header>
         <Modal.Body>Please confirm to continue</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShow("close")}>
+          <Button variant="secondary" onClick={() => setShow(showStates.CLOSE)}>
             Close
           </Button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => {uploadtoInstagram(props.token, props.pageID, props.image, props.caption, () => setShow("uploaded"));}}>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" onClick={() => {uploadtoInstagram(props.token, props.pageID, props.image, props.caption, () => setShow(showStates.UPLOADED));}}>
             Continue
           </button>
         </Modal.Footer>
       </Modal>
 
-      <Modal show={show === "uploaded"} onHide={() => setShow("close")}>
+      <Modal show={show === showStates.UPLOADED} onHide={() => setShow(showStates.CLOSE)}>
         <Modal.Header closeButton>
           <Modal.Title>Uploaded successfully!</Modal.Title>
         </Modal.Header>
         <Modal.Body>The image has been successfully uploaded</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShow("close")}>
+          <Button variant="secondary" onClick={() => setShow(showStates.CLOSE)}>
             Close
           </Button>
         </Modal.Footer>
