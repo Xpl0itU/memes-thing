0bc0408fd280ed92f9c97598f579910de8112a76
Xpl0itU <samuelcarballogarcia8@gmail.com>
2022-12-07 01:58:42 +0100

    Hopefully fix the reload bug

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
           <div className="pt-2"></div>
           <div className="flex space-x-2">
             <label style={{ paddingTop: '5px' }} htmlFor="subredditSelect">Subreddit: </label>
-            <Select id="subredditSelect" styles={subredditSelectStyle} isSearchable={false} options={subreddits} defaultValue={subreddits[0]} onChange={(e) => { setSub(e.value); reloadImages() }} />
+            <Select id="subredditSelect" styles={subredditSelectStyle} isSearchable={false} options={subreddits} defaultValue={subreddits[0]} value={sub} onChange={(e) => { setSub(e.value); reloadImages() }} />
           </div>
           <div className="pt-2" />
           <div className="flex space-x-10">
